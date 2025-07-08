439f9cd2f27077cec7f23098e83658bd51481d2d
HyeonProG <guswo3443@naver.com>
2025-07-08 14:26:14 +0900

    feat: participantManager 제거

--- src/main/java/back/vybz/chat_service/chat/application/ParticipantManager.java
+++
@@ -1,9 +0,0 @@
-package back.vybz.chat_service.chat.application;
-
-public interface ParticipantManager {
-
-    void registerParticipant(String chatRoomId, String participantUuid);
-
-    void unregisterParticipant(String chatRoomId, String participantUuid);
-
-}
